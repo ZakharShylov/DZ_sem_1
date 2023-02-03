a062653d704e60df610eb0e4f6f5627991938b11
«Zakhar <zakharshylov.2006@gmail.com>
2023-02-04 01:19:13 +0300

    rdy

---
+++ num_10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Clear();
+
+Console.Write("Введите трехзначное число ");
+
+int num = int.Parse(Console.ReadLine()!);
+ int res ;
+
+  if (num > 99 && num < 1000)
+{
+     res = num / 10 % 10;
+     Console.WriteLine($"Вторая цифра данного числа = {res}");
+} 
+  else   
+ { 
+      Console.WriteLine($"Введите трехзначное число ");
+}
+//  Console.WriteLine($"вторая цифра данного числа = {res}");
+
+// int res = a1 / 10 % 10;
+
+// Console.WriteLine($"Bторая цифра данного числа = {res}");
---
+++ num_15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+Console.Write("Введите номер дня недели ");
+int num = int.Parse(Console.ReadLine()!);
+ 
+ while ( num < 1 || num >7 )
+ {
+     Console.WriteLine($"Введите число от 1 до 7:  ");
+     num = int.Parse(Console.ReadLine()!);
+ }
+ if ( num == 6 || num == 7 )
+ {
+    Console.WriteLine($"Выходной ");
+ }
+ else 
+ {
+    Console.WriteLine($"Будний день ");
+ }
