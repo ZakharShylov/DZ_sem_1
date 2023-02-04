c6a85af6c70d3938f3c4c2eb161e1488efb0478c
«Zakhar <zakharshylov.2006@gmail.com>
2023-02-05 00:53:30 +0300

    rdy

---
+++ num_19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+Console.Clear();
+
+Console.Write("Введите пятизначное число ");
+int num1 = int.Parse(Console.ReadLine()!);
+int temp = num1;
+int num2 = 0;
+
+while ( temp !=0)
+{
+    num2 = num2 * 10 + temp % 10;
+    temp /= 10 ;
+}
+if (num1 == num2) 
+{
+    Console.WriteLine($"Да");
+}
+else 
+{
+    Console.WriteLine($"Нет");
+}
---
+++ num_21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.Write("Введите координату точки Xa: ");
+double Xa = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Введите координату точки Ya: ");
+double Ya = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Введите координату точки Za: ");
+double Za = Convert.ToDouble(Console.ReadLine()!);
+
+
+Console.Write("Введите координату точки Xb: ");
+double Xb = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Введите координату точки Yb: ");
+double Yb = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Введите координату точки Zb: ");
+double Zb = Convert.ToDouble(Console.ReadLine()!);
+
+double x = Math.Sqrt(Math.Pow(Xb - Xa, 2) + Math.Pow(Yb - Ya, 2) + Math.Pow(Zb - Za, 2));
+ Console.WriteLine($"Расстояние между двумя точками в пространстве равно {x}");
