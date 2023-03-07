e145523c0fe8bd86b0307d97abac01bf979ac6d7
«Zakhar <zakharshylov.2006@gmail.com>
2023-03-07 09:55:38 +0100

    rdy

---
+++ num_64/Program.cs
@@ -0,0 +1,13 @@
+﻿void WriteNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        WriteNumbers(m + 1, n);
+        Console.Write($"{m} ");
+    }
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+WriteNumbers(m, n);
